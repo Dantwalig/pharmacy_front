8b4507f9b430222bb45c33769719443250a5c404
Robert <r.niyonkuru@alustudent.com>
2026-01-30 17:12:28 +0200

    feat: Middleware to handle the routes securely

---
+++ middleware.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get('accessToken')?.value;
+  const userRole = request.cookies.get('userRole')?.value;
+
+  const { pathname } = request.nextUrl;
+
+  const isSuperAdminRoute = pathname.startsWith('/super-admin');
+  const isPharmacyRoute = pathname.startsWith('/pharmacy');
+
+  if (isSuperAdminRoute) {
+    if (!token || userRole !== 'SUPER_ADMIN') {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+  if (isPharmacyRoute) {
+    if (!token || userRole !== 'PHARMACY') {
+      return NextResponse.redirect(new URL('/login', request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ['/super-admin/:path*', '/pharmacy/:path*'],
+};
